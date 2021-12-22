6fabff6fb1ef12cd2e0234cf6c61b2e168ba4f4f
Naimerith <naimerith89@gmail.com>
2021-12-22 17:21:13 -0500

    funcion para validad la ruta

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-module.exports = () => {
-  // ...
-};
+const fs = require ('fs');
+const path = require ('path'); 
+
+////////COMO SABER SI LA RUTA EXISTE/////////
+const pathExists = function(ruta) {
+   return fs.existsSync(ruta)
+} 
+console.log(pathExists(process.argv[2]))

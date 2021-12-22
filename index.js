82bfb1b5b57e23fd49a0118c47b4e6ee532d393e
Naimerith <naimerith89@gmail.com>
2021-12-22 17:32:58 -0500

    Funcion que verifica si la ruta es absoluta o se convierte en una

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ const pathExists = function(ruta) {
    return fs.existsSync(ruta)
 } 
 console.log(pathExists(process.argv[2]))
+
+//////////COMO SABER SI LA RUTA ES ABSOLUTA///////////
+const pathIsAbsolute = function(ruta){
+  return path.isAbsolute(ruta)
+} 
+console.log(pathIsAbsolute(process.argv[2]));
+
+////////// CONVIERTO LA RUTA EN ABSOLUTA /////////// 
+const convertPathInAbsolute = function(ruta){
+  if (path.isAbsolute(ruta)){
+    return ruta
+  }else{
+    return path.resolve(ruta)
+  }
+}
+console.log(convertPathInAbsolute(process.argv[2]));

5d2290498aad5ba58939a3bc06b404c3ba9c4c8d
Naimerith <naimerith89@gmail.com>
2022-01-07 17:13:45 -0500

    Se listan las carpetas de forma recursiva

--- index.js
+++ index.js
@@ -1,24 +1,47 @@
-const fs = require ('fs');
-const path = require ('path'); 
+const fs = require('fs');
+const path = require('path');
 
 ////////COMO SABER SI LA RUTA EXISTE/////////
-const pathExists = function(ruta) {
-   return fs.existsSync(ruta)
-} 
+const pathExists = function (ruta) {
+  return fs.existsSync(ruta)
+}
 console.log(pathExists(process.argv[2]))
 
-//////////COMO SABER SI LA RUTA ES ABSOLUTA///////////
-const pathIsAbsolute = function(ruta){
-  return path.isAbsolute(ruta)
-} 
-console.log(pathIsAbsolute(process.argv[2]));
-
 ////////// CONVIERTO LA RUTA EN ABSOLUTA /////////// 
-const convertPathInAbsolute = function(ruta){
-  if (path.isAbsolute(ruta)){
+const convertPathInAbsolute = (ruta) => {
+  if (path.isAbsolute(ruta)) {
     return ruta
-  }else{
-    return path.resolve(ruta)
   }
+  return path.resolve(ruta)
 }
 console.log(convertPathInAbsolute(process.argv[2]));
+
+//////////PREGUNTO SI ES UN DIRECTORIO///////////
+const pathIsDirectory = (ruta) => {
+  return fs.lstatSync(ruta).isDirectory()
+}
+console.log(pathIsDirectory(process.argv[2]));
+
+////////PREGUNTO SI ES UN ARCHIVO//////////
+const pathIsFile = function (ruta) {
+  return fs.statSync(ruta).isFile()
+}
+console.log(pathIsFile(process.argv[2])); //retorna un booleano 
+
+//////////LEER DIRECTORIO O ARCHIVO DE FORMA RECURSIVA///////////
+const readDirectoryandFile = (ruta) => {
+  let arrayResult = []; 
+  if(pathIsDirectory(ruta)){
+    const arrayDirectory = fs.readdirSync(ruta); 
+    arrayDirectory.forEach(archivo => {
+      const routeList = path.join(ruta, archivo); 
+      arrayResult = arrayResult.concat(readDirectoryandFile(routeList)) 
+    } 
+  )}else{
+    arrayResult.push(ruta) 
+  }
+  return arrayResult; 
+}
+console.log (readDirectoryandFile(process.argv[2]));
+
+
